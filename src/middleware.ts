203614c0b296430f1008c319769575a1a5820d8f
HoanIT <lehoan.dev@gmail.com>
2024-11-09 03:21:39 +0700

    [feat]: use new feature of i18n

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,36 +1,39 @@
 import { NextResponse } from "next/server";
 import createMiddleware from "next-intl/middleware";
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
-import { defaultLocale, locales, localePrefix, pathnames } from "@/lib/config";
+
+import { defaultLocale, locales, localePrefix } from "@/i18n/config";
 
 const intlMiddleware = createMiddleware({
   locales,
   defaultLocale,
   localePrefix,
-  pathnames,
 });
 
-const isProtectedRoute = createRouteMatcher(["/app(.*)"]);
+const isProtectedRoute = createRouteMatcher(["learn/(.*)"]);
 
 export default clerkMiddleware(
-  (auth, req) => {
+  async (auth, req) => {
     if (isProtectedRoute(req)) {
       req.headers.set("x-app-route", "true");
-      auth.protect();
+      await auth.protect();
       return NextResponse.next({ headers: req.headers });
     }
 
     return intlMiddleware(req);
-  },
-  { debug: process.env.NODE_ENV === "development" }
+  }
+  // { debug: process.env.NODE_ENV === "development" }
 );
 
 export const config = {
   matcher: [
-    "/",
-    "/:locale",
-    "/app/:path*",
-    "/((?!_next|_vercel|.*\\..*).*)",
+    // "/",
+    // "/:locale",
+    // "/learn/:path*",
+
+    // Skip Next.js internals and all static files, unless found in search params
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    // Always run for API routes
     "/(api|trpc)(.*)",
   ],
 };
