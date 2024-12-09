13bf238a32da8b71c72f9e1df6e7367bf17abd8f
Hoan IT <lehoan.dev@gmail.com>
2024-12-09 17:21:45 +0700

    [feat]: add FlashCardNumber (LearnPage)

---
+++ src/components/swiper/FlashCardNumber.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import n2words from "n2words";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { EffectCards } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/effect-cards";
+import "@/styles/swiper.css";
+import { useParams } from "next/navigation";
+
+const FlashCardNumber = () => {
+  const params = useParams<{ locale: string }>();
+  const numbers = Array.from({ length: 10 }, (_, index) => index + 1);
+
+  return (
+    <div className="w-full font-din">
+      <Swiper
+        effect={"cards"}
+        grabCursor={true}
+        modules={[EffectCards]}
+        className="w-[240px] h-[320px]"
+      >
+        {numbers.map((value, index) => (
+          <SwiperSlide key={index} className="rounded-xl">
+            <div className="h-full flex flex-col justify-center items-center">
+              <p className="font-bold text-[200px] leading-[200px]">{value}</p>
+              <p className="text-3xl">{n2words(value, { lang: "vi" })}</p>
+              <p className="text-1xl">
+                ({n2words(value, { lang: params.locale })})
+              </p>
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
+};
+
+export default FlashCardNumber;
